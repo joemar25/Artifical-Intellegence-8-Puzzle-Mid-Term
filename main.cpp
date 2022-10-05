d2d8d6786cce2dc112971cb0a82bec884b00c376
joemar25 <joemar.cardino000@gmail.com>
2022-10-05 14:45:43 +0800

    Update

--- main.cpp
+++ main.cpp
@@ -1,90 +1,52 @@
+/**
+ *
+ * @file main.cpp
+ * @authors  - Balagtas, Arrlee
+ *           - Cardiño, Joemar
+ *           - Gomez, Olan
+ * @brief
+ * @version 0.1
+ * @date 2022-10-05
+ *
+ * @copyright Copyright (c) 2022
+ *
+ */
+
 #include <iostream>
 #include <string>
-#include <ctime>
 
-void configPrint(int puzzle[3][3], std::string difficulty)
+#define ROW_COL 3
+unsigned int ROW, COL;
+
+class Puzzle
 {
-    std::cout << difficulty << std::endl;
-    for (int r = 0; r < 3; r++)
+public:
+    void set(int old_position, int new_position)
     {
-        for (int c = 0; c < 3; c++)
-            std::cout << puzzle[r][c] << " ";
+    }
+
+    void Display(int goal[ROW_COL][ROW_COL])
+    {
+        for (ROW = 0; ROW < ROW_COL; ROW++)
+        {
+            for (COL = 0; COL < ROW_COL; COL++)
+                std::cout << ROW + COL << " ";
+            std::cout << std::endl;
+        }
         std::cout << std::endl;
     }
-    std::cout << std::endl;
-}
+};
 
 int main()
 {
 
-    // default variables
-    char input;
-
-    // goal state
-    int goal[3][3] = {
+    int goal[ROW_COL][ROW_COL] = {
         {1, 2, 3},
         {8, 0, 4},
         {7, 6, 5}};
 
-    // easy
-    int easyP[3][3] = {
-        {1, 3, 4},
-        {5, 6, 2},
-        {7, 0, 5}};
-
-    // medium
-    int mediumP[3][3] = {
-        {1, 3, 4},
-        {5, 6, 2},
-        {7, 0, 5}};
-
-    // hard
-    int hardP[3][3] = {
-        {1, 3, 4},
-        {5, 6, 2},
-        {7, 0, 5}};
-
-    // worst
-    int worstP[3][3] = {
-        {1, 3, 4},
-        {5, 6, 2},
-        {7, 0, 5}};
-
-    // default
-    int defaultP[3][3] = {
-        {1, 3, 4},
-        {5, 6, 2},
-        {7, 0, 5}};
-
-    configPrint(easyP, "a. easy");
-    configPrint(mediumP, "b. medium");
-    configPrint(hardP, "c. hard");
-    configPrint(worstP, "d. worst");
-    configPrint(defaultP, "e. default");
-
-    // promting
-    std::cout << "choose a puzzle to solve...\n -select a letter and press enter: ";
-    std::cin >> input;
-    std::cout << std::endl;
-
-    if (input == 'a')
-    {
-        std::cout << "hello " << input << std::endl;
-    }
-    if (input == 'b')
-    {
-        std::cout << "hello " << input << std::endl;
-    }
-    if (input == 'c')
-    {
-        std::cout << "hello " << input << std::endl;
-    }
-    if (input == 'd')
-    {
-        std::cout << "hello " << input << std::endl;
-    }
-
-    // std::cout << <<std::endl;
+    Puzzle puzzle;
+    puzzle.Display(goal);
 
     return 0;
 }
---
+++ test/class.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+
+// object Puzzle
+class Puzzle
+{
+
+    /*
+        members of this class is private by default,
+        but putting them inside the public makes them visible
+    */
+
+public:
+    int number;
+};
+
+int main()
+{
+    // instance of object Puzzle
+    Puzzle a;
+
+    return 0;
+}
