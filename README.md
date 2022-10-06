59f47cda09d1926a1744d784e23d4111aaa76bde
joemar25 <joemar.cardino000@gmail.com>
2022-10-06 11:21:34 +0800

    DisplayFunctionFixed

--- README.md
+++ README.md
@@ -9,3 +9,18 @@ Project Documentation
 // number of nodes expanded before reaching the goal [a -> c -> f]
 
 - if has an input (if ever) check if input data is duplicated
+- inspired output : <https://www.youtube.com/watch?v=7L68-i_Ob5k&ab_channel=OscarVillasana>
+
+my pattern
+
+/*
+
++---+---+---+
+| 1 | 2 | 3 |
++---+---+---+
+| 4 |   | 8 |
++---+---+---+
+| 5 | 7 | 6 |
++---+---+---+
+
+*/
--- main.cpp
+++ main.cpp
@@ -13,9 +13,10 @@
 #include <iostream>
 #include <string>
 #include <ctime>
-#define ROW_COL 3
+#define ROW 3
+#define COL 3
 
-unsigned int ROW, COL;
+unsigned int COUNTER, _j, _i;
 
 class Node
 {
@@ -26,33 +27,28 @@ class Node
 class Puzzle
 {
     // class variables - hidden by default
-    unsigned int x, y;
-    unsigned int puzzleValue[ROW_COL][ROW_COL];
+    unsigned int puzzleValue[ROW * COL];
 
 public:
     // to initialize outside this class
-    Puzzle(int[][ROW_COL]);          // default constructor
-    int PuzzleValue(int, int) const; // get Puzzle's Current state
-    bool IsGoal();                   // check if Current State is the Goal State
-    void PrintBoard();               // print the Current State
+    Puzzle(int[]);              // default constructor
+    int PuzzleValue(int) const; // get Puzzle's Current state
+    bool IsGoal();              // check if Current State is the Goal State
+    void PrintBoard();          // print the Current State
 
 private:
     // private function, accesable only inside this class
-
     bool isValid(int num)
     {
         return !((num > 8) || (num < 0));
     }
 
-    void setPuzzle(int puzzle[ROW_COL][ROW_COL])
+    void setPuzzle(int puzzle[])
     {
-        for (ROW = 0; ROW < ROW_COL; ROW++)
+        for (COUNTER = 0; COUNTER < (ROW * COL); COUNTER++)
         {
-            for (COL = 0; COL < ROW_COL; COL++)
-            {
-                if (isValid(puzzle[ROW][COL]))
-                    this->puzzleValue[ROW][COL] = puzzle[ROW][COL];
-            }
+            if (isValid(puzzle[COUNTER]))
+                this->puzzleValue[COUNTER] = puzzle[COUNTER];
         }
     }
 };
@@ -62,79 +58,71 @@ int main()
 {
     clock_t start, end;
 
-    int _puzzl1[ROW_COL][ROW_COL] = {
-        {1, 2, 3},
-        {8, 0, 4},
-        {7, 6, 5}};
-
-    int _puzzl2[ROW_COL][ROW_COL] = {
-        {0, 2, 4},
-        {8, 1, 7},
-        {3, 6, 5}};
+    int _puzzl1[ROW * COL] = {1, 2, 3, 8, 0, 4, 7, 6, 5};
+    int _puzzl2[ROW * COL] = {0, 2, 4, 8, 1, 7, 3, 6, 5};
 
     Puzzle puzzle1(_puzzl1);
     Puzzle puzzle2(_puzzl2);
 
-    // puzzle 1 test
     {
         start = clock();
         puzzle1.PrintBoard();
         end = clock();
 
         std::cout << "T(N) = " << ((double)(end - start)) / CLOCKS_PER_SEC << std::endl;
-        std::cout << "Is Goal? " << puzzle1.IsGoal() << "\n\n";
-    }
-
-    // puzzle 2 test
-    {
-        start = clock();
-        puzzle2.PrintBoard();
-        end = clock();
-
-        std::cout << "T(N) = " << ((double)(end - start)) / CLOCKS_PER_SEC << std::endl;
-        std::cout << "Is Goal? " << puzzle2.IsGoal() << std::endl;
+        std::cout << "Is Goal? " << puzzle1.IsGoal() << "";
     }
 
     return 0;
 }
 
 /* puzzle public functions */
-Puzzle::Puzzle(int puzzle[ROW_COL][ROW_COL])
+Puzzle::Puzzle(int puzzle[])
 {
     this->setPuzzle(puzzle);
 }
 
 bool Puzzle::IsGoal()
 {
-    int goal[ROW_COL][ROW_COL] = {
-        {1, 2, 3},
-        {8, 0, 4},
-        {7, 6, 5}};
+    int goal[ROW * COL] = {1, 2, 3, 8, 0, 4, 7, 6, 5};
 
-    for (ROW = 0; ROW < ROW_COL; ROW++)
+    for (COUNTER = 0; COUNTER < (ROW * COL); COUNTER++)
     {
-        for (COL = 0; COL < ROW_COL; COL++)
-        {
-            if (this->PuzzleValue(ROW, COL) != goal[ROW][COL]) // if any two same positioned items not equal
-                return false;                                  // this state is not the goal
-        }
+        if (this->PuzzleValue(COUNTER) != goal[COUNTER]) // if any two same positioned items not equal
+            return false;                                // this state is not the goal
     }
     return true;
 }
 
-int Puzzle::PuzzleValue(int _row, int _col) const
+int Puzzle::PuzzleValue(int _pos) const
 {
-    return this->puzzleValue[_row][_col];
+    return this->puzzleValue[_pos];
 }
 
 void Puzzle::PrintBoard()
 {
-    for (ROW = 0; ROW < ROW_COL; ROW++)
+    COUNTER = 0;
+    // std::cout << " " << this->PuzzleValue(COUNTER) << " *";
+    for (_i = 0; _i < (ROW); _i++)
     {
-        for (COL = 0; COL < ROW_COL; COL++)
-            std::cout << this->PuzzleValue(ROW, COL) << " ";
-        std::cout << std::endl;
+        for (_j = 0; _j < COL; _j++)
+            std::cout << "+---";
+        std::cout << "+" << std::endl;
+
+        for (_j = 0; _j < COL; _j++)
+        {
+            // to string is just temporary
+            std::string result = (this->PuzzleValue(COUNTER) == 0) ? " " : std::to_string(this->PuzzleValue(COUNTER));
+            std::cout << "| " << result << " ";
+            COUNTER++;
+        }
+
+        std::cout << "|" << std::endl;
     }
+
+    for (_j = 0; _j < COL; _j++)
+        std::cout << "+---";
+    std::cout << "+" << std::endl;
 }
 
 // other class functions
